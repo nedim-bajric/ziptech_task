8e48597871f0bbd42e94b35dae105347b683d2a0
Bajric Nedim <76698338+nedim-bajric@users.noreply.github.com>
2022-05-16 13:51:03 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "swiper/css";
 import App from "./App";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import movieReducer from "./Slices/movieSlice";
-
-export const store = configureStore({
-  reducer: {
-    movie: movieReducer,
-  },
-});
